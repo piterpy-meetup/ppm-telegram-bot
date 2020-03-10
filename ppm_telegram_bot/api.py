cb4eb1d77faefed27d7a2c6ca7180fcafddad3f1
Boger <kotvberloge@gmail.com>
2020-03-10 11:04:19 +0300

    :fire: Remove old noted

--- ppm_telegram_bot/api.py
+++ ppm_telegram_bot/api.py
@@ -22,10 +22,3 @@ app.include_router(triggers.router, prefix="/triggers", tags=["triggers"])
 for route in app.routes:
     if isinstance(route, APIRoute):
         route.operation_id = route.name
-
-# TODO: how to generate http-client via https://github.com/dmontagu/fastapi_client
-# ../fastapi_client/scripts/generate.sh -i ~/Downloads/openapi\ \(1\).json -p ppm_telegram_bot_client -o ../ppm-telegram-bot
-# from ppm_telegram_bot_client.api_client import ApiClient, AsyncApis
-# client = ApiClient(host="http://localhost:8000")
-# async_apis = AsyncApis(client)
-# await async_apis.triggers_api.notion_error()
