74807ca0441b271ca5336eec65c80479bc1bab46
Boger <kotvberloge@gmail.com>
2020-03-16 15:45:12 +0300

    test headers

--- run-app.sh
+++ run-app.sh
@@ -5,7 +5,6 @@ PORT=${2:-8000}
 
 exec 2>&1 gunicorn \
   ppm_telegram_bot.api:app \
-  --errorlog - \
   --capture-output \
   --workers=$WORKERS_NUM \
   --worker-class=uvicorn.workers.UvicornWorker \
