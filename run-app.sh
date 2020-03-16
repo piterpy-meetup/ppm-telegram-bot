fe777e38610c6f9c6aa02b4d98d9b43c8db3695b
Boger <kotvberloge@gmail.com>
2020-03-16 17:45:28 +0300

    :ambulance: Fix forwarding IPs in docker container for security reasons
    Fixes #16

--- run-app.sh
+++ run-app.sh
@@ -3,4 +3,12 @@
 WORKERS_NUM=${1:-1}
 PORT=${2:-8000}
 
-exec 2>&1 gunicorn ppm_telegram_bot.api:app --workers=$WORKERS_NUM --worker-class=uvicorn.workers.UvicornWorker --bind=0.0.0.0:$PORT
+# see https://github.com/encode/uvicorn/issues/589
+export FORWARDED_ALLOW_IPS="*"
+
+exec 2>&1 gunicorn \
+  ppm_telegram_bot.api:app \
+  --forwarded-allow-ips="*" \
+  --workers=$WORKERS_NUM \
+  --worker-class=uvicorn.workers.UvicornWorker \
+  --bind=0.0.0.0:$PORT
